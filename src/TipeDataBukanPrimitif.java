57037a728354a7dc77eeacc125b3ff8a962f0e91
Fahrul Fauzi <fahrul.fauz@gmail.com>
2021-09-11 14:44:58 +0700

    Tipe Data Bukan Primitif

---
+++ src/TipeDataBukanPrimitif.java
@@ -0,0 +1,13 @@
+public class TipeDataBukanPrimitif {
+    public static void main(String[] args) {
+        Integer iniInteger = 100;
+        Long iniLong = 1000L;
+        System.out.println(iniInteger);
+        System.out.println(iniLong);
+
+        Byte iniByte = null;
+        System.out.println(iniByte);
+        iniByte = 100;
+        System.out.println(iniByte);
+    }
+}
