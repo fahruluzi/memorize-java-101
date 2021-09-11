ff4660f549d8290f31cca219871fe30a5241704b
Fahrul Fauzi <fahrul.fauz@gmail.com>
2021-09-11 14:38:01 +0700

    Tipe Data Number

---
+++ src/TipeDataNumber.java
@@ -0,0 +1,18 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte typeByte = 100;
+        short typeShort = 10_000;
+        int typeInt = 1_000_000_000;
+        long typeLong = 1_000_000_000;
+
+        float typeFloat = 1_000_000_000;
+        double typeDouble = 1_000_000_000;
+
+        System.out.println(typeByte);
+        System.out.println(typeShort);
+        System.out.println(typeInt);
+        System.out.println(typeLong);
+        System.out.println(typeFloat);
+        System.out.println(typeDouble);
+    }
+}
